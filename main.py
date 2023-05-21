d3469dfe0a76157ec48111bd748e30c401d836d2
unknown <v.tsvykh0@gmail.com>
2023-05-21 18:14:29 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+"Game: Student day"
+Group:
+Tsvykh Viktoria
+Fishchukova Sofia
+'''
+
+import ru_local as ru
+import random as random
+
+# Monday
+
+# Tuesday
+
+# Wednesday
+
+# Thursday
+
+# Friday
+
+# Saturday
+
+# Result
